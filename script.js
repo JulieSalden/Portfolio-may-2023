f8a3d32bbd7c8e019f8f137ec00c6af85c3f2304
JulieSalden <108523952+JulieSalden@users.noreply.github.com>
2023-05-20 13:33:51 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const toggleMenu = document.querySelector("#toggleMenu");
+const getProjectToggle = document.querySelector("#projectMenu");
+
+toggleMenu.addEventListener("mouseenter", function () {
+  getProjectToggle.style.transform = "translateX(0px)";
+});
+
+toggleMenu.addEventListener("mouseover", function () {
+  getProjectToggle.style.transform = "translateX(0px)";
+});
+
+toggleMenu.addEventListener("mouseleave", function () {
+  getProjectToggle.style.transform = "translateX(-325px)";
+});
+
+toggleMenu.addEventListener("click", function () {
+  getProjectToggle.style.transform = "translateX(-325px)";
+});
+
+getProjectToggle.addEventListener("mouseenter", function () {
+  getProjectToggle.style.transform = "translateX(0px)";
+});
+
+getProjectToggle.addEventListener("mouseover", function () {
+  getProjectToggle.style.transform = "translateX(0px)";
+});
+
+getProjectToggle.addEventListener("mouseleave", function () {
+  getProjectToggle.style.transform = "translateX(-325px)";
+});
+
+getProjectToggle.addEventListener("click", function () {
+  getProjectToggle.style.transform = "translateX(-325px)";
+});
